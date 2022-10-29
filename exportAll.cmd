707eb61673a28cc5d048ee8fb6a7613b2f3f6382
Jonathan Wilkinson <wjonathan@ultradent.com>
2022-10-29 11:50:40 -0600

    Adding STLs and updating presets

---
+++ exportAll.cmd
@@ -0,0 +1,23 @@
+openscad -q -o .\stl\M2_5.stl -p NutSorter.json -P M2.5 NutSorter.scad
+openscad -q -o .\stl\M3.stl -p NutSorter.json -P M3 NutSorter.scad
+openscad -q -o .\stl\M4.stl -p NutSorter.json -P M4 NutSorter.scad
+openscad -q -o .\stl\M5.stl -p NutSorter.json -P M5 NutSorter.scad
+openscad -q -o .\stl\M6.stl -p NutSorter.json -P M6 NutSorter.scad
+openscad -q -o .\stl\M8.stl -p NutSorter.json -P M8 NutSorter.scad
+
+openscad -q -o .\stl\Machine10.stl -p NutSorter.json -P Machine_10  NutSorter.scad
+openscad -q -o .\stl\Machine8.stl -p NutSorter.json -P Machine_8  NutSorter.scad
+openscad -q -o .\stl\Machine6.stl -p NutSorter.json -P Machine_6  NutSorter.scad
+openscad -q -o .\stl\Machine4.stl -p NutSorter.json -P Machine_4  NutSorter.scad
+openscad -q -o .\stl\Machine3.stl -p NutSorter.json -P Machine_3  NutSorter.scad
+
+
+openscad -q -o .\stl\I_1_4th.stl -p NutSorter.json -P Imperial_1_4  NutSorter.scad
+rem openscad -q -o .\stl\I_5_16ths.stl -p NutSorter.json -P I_5_16ths   NutSorter.scad
+rem openscad -q -o .\stl\I_3_8ths.stl -p NutSorter.json -P I_3_8ths   NutSorter.scad
+rem openscad -q -o .\stl\I_7_16ths.stl -p NutSorter.json -P I_7_16ths   NutSorter.scad
+rem openscad -q -o .\stl\I_Half_Inch.stl -p NutSorter.json -P I_Half_Inch   NutSorter.scad
+rem openscad -q -o .\stl\I_9_16ths.stl -p NutSorter.json -P I_9_16ths   NutSorter.scad
+rem openscad -q -o .\stl\I_5_8ths.stl -p NutSorter.json -P I_5_8ths   NutSorter.scad
+rem openscad -q -o .\stl\I_3_4ths.stl -p NutSorter.json -P I_3_4ths   NutSorter.scad
+rem openscad -q -o .\stl\I_7_8ths.stl -p NutSorter.json -P I_7_8ths   NutSorter.scad
